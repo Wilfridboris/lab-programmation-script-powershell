88442e37d4c0763fec671fb59fcf213e8fff7180
willfrid boris <boriskwayep35@yahoo.com>
2020-04-10 23:01:35 -0400

    0

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -10,7 +10,21 @@
     Derniere mise Ã  jour: yyyy-mm-dd
 
 #>
+$villes = "Toronto","Mississauga","Scarborough","Brampton"
+for ($i = 0; $i -le $villes.Count; $i = $i + 1) {
+    $villes[$i]
+}
 
-$personneNom = "Alice"
-$personneAge = 35
+Clear-Host
+"0"-F ($villes.Count % 2)
+
+[String]$personneNom = "Alice"
+[int]$personneAge = 35
 Write-Host " hi bonjour $personneNom,tu as $personneAge ans."
+# Test sur l'age de la personne
+switch ($personneAge) {
+    {$_ -le 25} {Write-Host 'en pleine jeunesse'; break }
+    {$_ -le 35} {Write-Host 'en pleine force vive'; break }
+    {$_ -le 45} {Write-Host 'en pleine maturité'; break }
+    Default {Write-Host 'en pleine serénité'}
+}
