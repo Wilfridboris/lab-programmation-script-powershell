cbdef2a676c480b34a1a2f9d00c46481a466b1b5
willfrid boris <boriskwayep35@yahoo.com>
2020-04-10 23:48:39 -0400

    Tutoriel complété

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -13,9 +13,20 @@
 
 # Definition de la fonction
 function Stagiaire {
+[CmdletBinding()]
+param (
+        [String]$personneNom,
+        [Int]$personneAge
+       
+    )
+ BEGIN {Write-Verbose "Début du script"}
+ PROCESS{ "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge}
+ END {Write-Verbose "Fin du script"}
+
 
 }
 
 # Appel de la fonction
-Stagiaire
+Stagiaire Toronto 35
+Stagiaire "Pascal Siakam" 26  -verbose
 
